9fb47bf197c2a6cdae807afb698e046af21a4036
Jami Protect <timmiejtwitch@gmail.com>
2026-08-12 00:15:59 +0200

    Add files via upload

---
+++ assets/js/config-v6.js
@@ -0,0 +1,17 @@
+window.JAMI_CONFIG = {
+  programName: "JaMi Protect",
+  githubOwner: "jami-protect",
+  githubRepo: "jami-protect",
+  installerFileName: "JaMiProtect_Setup.exe",
+  versionLabel: "Preview",
+  releaseReady: false,
+  pagesUrl: "https://jami-protect.github.io/jami-protect/",
+  githubUrl: "https://github.com/jami-protect/jami-protect"
+};
+(function(){
+ const c=window.JAMI_CONFIG;
+ c.repoUrl=`https://github.com/${c.githubOwner}/${c.githubRepo}`;
+ c.releasesUrl=`${c.repoUrl}/releases`;
+ c.latestReleaseUrl=`${c.releasesUrl}/latest`;
+ c.directInstallerUrl=`${c.latestReleaseUrl}/download/${c.installerFileName}`;
+})();
